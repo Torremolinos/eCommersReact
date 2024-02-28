5ae363ed0f2c7772df8b1e815f9c9fcd0af8fa74
Torremolinos <adrian_2054@hotmail.com>
2024-02-28 09:07:42 +0100

    Primeras pruebas del proyecto

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import { API_URL } from "../constants/env";
+
+function App() {
+  return (
+    <div>
+      <h1>Bienvenido </h1>
+      <p>Estas en {API_URL}</p>
+    </div>
+  );
+}
+
+export default App;
