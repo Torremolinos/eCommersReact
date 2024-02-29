ebddecef132b28b3b9b5e38d314377d7c39d5b17
Torremolinos <adrian_2054@hotmail.com>
2024-02-29 12:53:53 +0100

    Creamos error404

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+/*hay que quitar los imports de react*/
 
 export const Error404 = () => {
   return (
-    <div>Error404</div>
-  )
-}
+    <>
+      <div>Error404</div>
+      <p>La página no existe</p>
+    </> /*normalmente solo puedes mandar un parametro pero debe estar entre divs o fragments </>*/
+  );
+};
 
 //rfac -> arrow function
 
