e0e8d2309765284f9f031ece1c34972bceeb4173
Torremolinos <adrian_2054@hotmail.com>
2024-03-04 09:55:34 +0100

    instalacion del prettier y su configuracion en eslint.crsf

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,26 +1,38 @@
 import axios from "axios";
 import { API_URL } from "../../constants/env";
 import { setToken } from "../../helpers/auth";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  //Utilizar redirecciones de React Router
 
-    const handleSubmit =(e) => {
-        e.preventDefault();
-        const data = {
-            email: e.target.email.value,
-            password: e.target.password.value
-        }
-        axios.post(`${API_URL}/public/login`, data)
-        .then(response => setToken(response.data.data.token))
-        .catch(error => console.log(error));
-        
-    }
+  const nav = useNavigate();
+
+  //Manejo del estado de errores
+
+  const [error, setError] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setError(); //Limpiar errores previos en caso de que existan
+    const data = {
+      email: e.target.email.value,
+      password: e.target.password.value,
+    };
+    axios
+      .post(`${API_URL}/public/login`, data)
+      .then((response) => {
+        setToken(response.data.data.token); //Guardar el token en el local storage
+        nav("/"); //Redireccionar a la página de inicio
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div className="pt-16 max-w-256 m-auto">
       <form onSubmit={handleSubmit}>
-        <input type="email" name="email" placeholder="Correo Electrónico"/>
-        <input type="password" name="password" placeholder="Contraseña"/>
+        <input type="email" name="email" placeholder="Correo Electrónico" />
+        <input type="password" name="password" placeholder="Contraseña" />
         <button type="submit">Ingresar</button>
       </form>
     </div>
