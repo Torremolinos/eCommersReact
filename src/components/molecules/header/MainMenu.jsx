ca6cd2afb0e55710d8d6cd3f83907a8d9a9f18e4
Torremolinos <adrian_2054@hotmail.com>
2024-03-08 12:05:17 +0100

    Incluyo el ternario del mainmenu.jsx

--- src/components/molecules/header/MainMenu.jsx
+++ src/components/molecules/header/MainMenu.jsx
@@ -1,6 +1,16 @@
 import { Link } from "react-router-dom";
+import { token } from "../../../helpers/auth";
+import { useNavigate } from "react-router-dom";
+import { deleteToken } from "../../../helpers/auth";
 
 const MainMenu = () => {
+  const nav = useNavigate();
+
+  const handleSession = () => {
+    deleteToken(); //Borrar el token del local storage
+    nav("/login");
+  };
+
   return (
     <nav className="w-full">
       <ul className="flex justify-end text-gray-100">
@@ -14,11 +24,19 @@ const MainMenu = () => {
             Productos
           </Link>
         </li>
-        <li className="flex items-center">
-          <Link className="menu-item" to="/login">
-            Iniciar sesión
-          </Link>
-        </li>
+        {!token() ? (
+          <li className="flex items-center">
+            <Link className="menu-item" to="/login">
+              Iniciar sesión
+            </Link>
+          </li>
+        ) : (
+          <li className="flex items-center">
+            <a onClick={handleSession} className="menu-item cursor-pointer">
+              Cerrar sesión
+            </a>
+          </li>
+        )}
       </ul>
     </nav>
   );
